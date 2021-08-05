c4c1c183cafcb7980c4a7b592dc5ae66b56fa05a
Abdul Florez <abdulflorez1524@gmail.com>
2021-08-04 23:47:35 -0500

    updated to react router, cards of products, css for pages and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-import './index.css';
-import Header from './components/header/Header'
+import "./styles/index.css";
+import { BrowserRouter, Route,  Switch } from "react-router-dom";
+
+import Header from "./components/header/Header";
+import Shop from "./pages/shop/Shop";
+import Redeems from "./pages/redeems/Redeems";
+import Coins from "./pages/coins/Coins";
+
+import { UserProvider } from "./context/UserProvider";
+import { ProductProvider } from "./context/ProductProvider";
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      HELLO WORLD!
-    </div>
+    <BrowserRouter>
+      <ProductProvider>
+        <UserProvider>
+          <div className="App">
+            <Header />
+            <main>
+              <Switch>
+              <Route exact path="/" component={Shop} />
+              <Route exact path="/orders" component={Redeems} />
+              <Route exact path="/getcoins" component={Coins} />
+              </Switch>
+            </main>
+          </div>
+        </UserProvider>
+      </ProductProvider>
+    </BrowserRouter>
   );
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,45 @@
 import "./header.css";
+import { UserContext } from "../../context/UserProvider";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
+  const { userData } = useContext(UserContext);
+  // getUser(setUserData)
+
   return (
     <header className="header">
       <nav className="header__nav">
-        <img
-          className="header__logo"
-          src="/assets/aerolab-logo.svg"
-          alt="logo-aerolab"
-        />
+        <ul className="header__navbar">
+          <li>
+            <Link to="/">
+              <img
+                className="header__navbar--logo"
+                src="/assets/aerolab-logo.svg"
+                alt="logo-aerolab"
+              />
+            </Link>
+          </li>
+          <li>
+            <Link className="header__navbar--options" to="/">
+              Shop
+            </Link>
+          </li>
+          <li>
+            <Link className="header__navbar--options" to="/orders">
+              Orders
+            </Link>
+          </li>
+          <li>
+            <Link className="header__navbar--options" to="/getcoins">
+              Get Coins
+            </Link>
+          </li>
+        </ul>
         <div className="header__userbox">
-          <p className="header__username">John Kile</p>
+          <p className="header__username">{userData.name}</p>
           <div className="header__balancebox">
-            <p className="header__balancebox--balance">2000</p>
+            <p className="header__balancebox--balance">{userData.points}</p>
             <img
               className="header__balancebox--icon"
               src="/assets/icons/coin.svg"
@@ -21,7 +48,7 @@ function Header() {
           </div>
         </div>
       </nav>
-      <img className="header__cover" src="/assets/header-x2.png" alt=""/>
+      <img className="header__cover" src="/assets/header-x1.png" alt="" />
       <h1 className="header__title">Electronics</h1>
     </header>
   );
